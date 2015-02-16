3265d36fad1fc1fbf2a7317394c2a93601dd5e97
Jarkko Miettinen <jarkko.miettinen@relex.fi>
2015-02-16 20:32:43 +0200

    Change JAXB package

--- src/main/java/fi/eonwe/wikilinks/WikiStreamer.java
+++ src/main/java/fi/eonwe/wikilinks/WikiStreamer.java
@@ -1,9 +1,13 @@
 package fi.eonwe.wikilinks;
 
+import fi.eonwe.wikilinks.jaxb.PageType;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 
 /**
  */
@@ -14,6 +18,24 @@ public class WikiStreamer {
         return stream;
     }
 
+    private static final ThreadLocal<JAXBContext> JAXB_CONTEXT_THREAD_LOCAL = new ThreadLocal<JAXBContext>() {
+        @Override
+        protected JAXBContext initialValue() {
+            try {
+                return JAXBContext.newInstance("fi.eonwe.wikilinks.jaxb");
+            } catch (JAXBException e) {
+                throw new AssertionError(e);
+            }
+        }
+    };
 
+
+    public static JAXBContext getJAXB() {
+        return JAXB_CONTEXT_THREAD_LOCAL.get();
+    }
+
+    public Iterator<PageType> readFrom(InputStream stream) {
+        return (1 < 2 ? null : null);
+    }
 
 }
