3dfa7750e5bcc010675dcfc791225afc48d78c3c
Jarkko Miettinen <jarkko.miettinen@relex.fi>
2015-02-17 09:52:30 +0200

    Streaming parser

--- src/main/java/fi/eonwe/wikilinks/App.java
+++ src/main/java/fi/eonwe/wikilinks/App.java
@@ -1,13 +1,65 @@
 package fi.eonwe.wikilinks;
 
+import fi.eonwe.wikilinks.jaxb.PageType;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.function.Consumer;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException, XMLStreamException {
+        String filePath = "src/data/simplewiki-20141222-pages-meta-current.xml";
+        if (args.length == 1) {
+            filePath = args[0];
+        }
+        InputStream in = new FileInputStream(filePath);
+        if (filePath.endsWith(".bz2")) {
+            in = new BZip2CompressorInputStream(in);
+        }
+        BufferedInputStream bis = new BufferedInputStream(in, 1 << 15);
+//        JAXBReadFrom(bis);
+        streamFrom(bis);
+    }
+
+    private static void streamFrom(InputStream stream) throws XMLStreamException {
+        int[] iters = new int[] { 0 };
+        long startTime = System.currentTimeMillis();
+        WikiStreamer.streamFrom(stream).forEach(page -> {
+            if ((++iters[0] & 32767) == 0) {
+                System.out.printf("%d articles so far%n", iters[0]);
+            }
+        });
+        System.out.printf("Read %d articles in %d ms%n", iters[0], System.currentTimeMillis() - startTime);
+    }
+
+    private static void JAXBReadFrom(InputStream stream) {
+        int[] iters = new int[] { 0 };
+        long startTime = System.currentTimeMillis();
+
+        WikiStreamer.readFrom(stream).forEach(new Consumer<PageType>() {
+            @Override
+            public void accept(PageType page) {
+                if ((++iters[0] & 32767) == 0) {
+                    System.out.printf("%d articles so far%n", iters[0]);
+                }
+//                if (page.getRedirect() != null) {
+//                    System.out.printf("%s -> %s%n", page.getTitle(), page.getRedirect().getTitle());
+//                } else {
+//                    System.out.printf("%s (ns = %d, id = %d)%n", page.getTitle(),
+//                            page.getNs(),
+//                            page.getId());
+//                }
+            }
+        });
+        System.out.printf("Read %d articles in %d ms%n", iters[0], System.currentTimeMillis() - startTime);
+
     }
 }
--- src/main/java/fi/eonwe/wikilinks/WikiStreamer.java
+++ src/main/java/fi/eonwe/wikilinks/WikiStreamer.java
@@ -1,13 +1,25 @@
 package fi.eonwe.wikilinks;
 
+import com.google.common.collect.ImmutableList;
+import fi.eonwe.wikilinks.jaxb.MediaWikiType;
 import fi.eonwe.wikilinks.jaxb.PageType;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  */
@@ -34,8 +46,97 @@ public class WikiStreamer {
         return JAXB_CONTEXT_THREAD_LOCAL.get();
     }
 
-    public Iterator<PageType> readFrom(InputStream stream) {
-        return (1 < 2 ? null : null);
+    public static Unmarshaller getUnmarshaller() {
+        try {
+            return JAXB_CONTEXT_THREAD_LOCAL.get().createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static Iterable<PageType> readFrom(InputStream stream) {
+        Unmarshaller unmarshaller = getUnmarshaller();
+        try {
+            JAXBElement root = (JAXBElement) unmarshaller.unmarshal(stream);
+            return ((MediaWikiType) root.getValue()).getPage();
+        } catch (JAXBException e) {
+            e.printStackTrace(System.err);
+            return Collections.emptyList();
+        }
+    }
+
+    private static Iterator<WikiPage> streamFrom(XMLStreamReader reader) {
+        return new Iterator<WikiPage>() {
+
+            private boolean advanced = false;
+            private WikiPage page = null;
+
+            @Override
+            public boolean hasNext() {
+                if (!advanced) advance();
+                return page != null;
+            }
+
+            @Override
+            public WikiPage next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("out of elements");
+                }
+                WikiPage toReturn = page;
+                advance();
+                return toReturn;
+            }
+
+            private void advance() {
+                boolean pageStarted = false;
+                String title = null;
+                long id = -1;
+                long ns = -1;
+                ImmutableList<String> links = null;
+                try {
+                    while(reader.hasNext()) {
+                        final int eventType = reader.next();
+                        if (eventType == XMLStreamConstants.START_ELEMENT) {
+                            switch (reader.getLocalName()) {
+                                case "page": pageStarted = true; break;
+                                case "title": title = reader.getElementText(); break;
+                                case "id": id = Long.parseLong(reader.getElementText()); break;
+                                case "ns": ns = Long.parseLong(reader.getElementText()); break;
+                                case "text": links = getLinks(reader.getElementText()); break;
+                            }
+                        } else if (eventType == XMLStreamConstants.END_ELEMENT && "page".equals(reader.getLocalName())) {
+                            assert title != null;
+                            assert id != -1;
+                            assert ns != -1;
+                            page = new WikiPage(title, id, ns, links);
+                            advanced = true;
+                            break;
+                        } else if (eventType == XMLStreamConstants.END_DOCUMENT) {
+                            eof();
+                            break;
+                        }
+                    }
+                } catch (XMLStreamException e) {
+                    eof();
+                }
+            }
+
+            private void eof() {
+                advanced = true;
+                page = null;
+            }
+
+        };
+    }
+
+    private static ImmutableList<String> getLinks(String wikiText) {
+        return ImmutableList.of();
+    }
+
+    public static Iterable<WikiPage> streamFrom(InputStream is) throws XMLStreamException {
+        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(is);
+        assert reader.getEventType() == XMLStreamConstants.START_DOCUMENT;
+        return () -> streamFrom(reader);
     }
 
 }
